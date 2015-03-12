2fae7235c373893ac0a5a87ffe17d55dcfede4fd
Alexander Guk <alex.guk.kpi@gmail.com>
2015-03-13 01:25:42 +0200

    Hello world commit

---
+++ android/src/com/github/alexanderguk/match3/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.alexanderguk.match3.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.alexanderguk.match3.Match3Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Match3Main(), config);
+	}
+}
---
+++ desktop/src/com/github/alexanderguk/match3/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.alexanderguk.match3.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.alexanderguk.match3.Match3Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Match3Main(), config);
+	}
+}
