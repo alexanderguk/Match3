ca13e026d3425a12039f8922f2f1567e480cc2b2
Alexander Guk <alex.guk.kpi@gmail.com>
2015-03-14 00:47:24 +0200

    Finished basic menu implementation

--- core/src/com/github/alexanderguk/match3/screen/ScreenGame.java
+++ core/src/com/github/alexanderguk/match3/screen/ScreenGame.java
@@ -2,6 +2,7 @@ package com.github.alexanderguk.match3.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -15,11 +16,13 @@ public class ScreenGame implements Screen {
 
     @Override
     public void show() {
-
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(delta);
         stage.draw();
     }
--- core/src/com/github/alexanderguk/match3/screen/ScreenMenu.java
+++ core/src/com/github/alexanderguk/match3/screen/ScreenMenu.java
@@ -1,35 +1,71 @@
 package com.github.alexanderguk.match3.screen;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.github.alexanderguk.match3.Match3Main;
+import com.sun.javafx.applet.Splash;
 
 public class ScreenMenu implements Screen {
     private Stage stage;
-
-    private BitmapFont font;
+    private Skin skin;
+    private Table table;
+    private TextButton buttonPlay;
+    private TextButton buttonExit;
+    private Label title;
 
     public ScreenMenu(SpriteBatch batch) {
-                font = new BitmapFont();
-        font.setColor(Color.RED);
-
         stage = new Stage(new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()), batch);
-        stage.addActor(new ButtonStart(font));
-        stage.addActor(new ButtonExit(font));
-        Gdx.input.setInputProcessor(stage);
+        skin = new Skin(Gdx.files.internal("skins/menuSkin.json"),
+                new TextureAtlas(Gdx.files.internal("skins/menuSkin.pack")));
+
+        table = new Table();
+        buttonPlay = new TextButton("Play", skin);
+        buttonExit = new TextButton("Exit", skin);
+        title = new Label("Match 3",skin);
     }
 
     @Override
     public void show() {
+        buttonPlay.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Match3Main.getInstance().showGame();
+            }
+        });
+        buttonExit.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Exit");
+                Gdx.app.exit();
+            }
+        });
 
+        table.add(title).padBottom(40).row();
+        table.add(buttonPlay).row();
+        table.add(buttonExit).row();
+
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(delta);
         stage.draw();
     }
@@ -56,7 +92,7 @@ public class ScreenMenu implements Screen {
 
     @Override
     public void dispose() {
-        font.dispose();
         stage.dispose();
+        skin.dispose();
     }
 }
